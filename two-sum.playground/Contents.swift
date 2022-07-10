2d7fd92cb8efdaca49469235b534738e22d51973
codermoe <codermoe@gmail.com>
2022-07-10 23:28:50 +0800

    feat: twosum

---
+++ two-sum.playground/Contents.swift
@@ -0,0 +1,25 @@
+/**
+ 要点：
+ 1. 使用map以target - num为key,以当前下标为value
+ 2. 循环到target - num时，读取之前的下标
+ 3. 将之前的下标和当前的下标返回
+ */
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+        for index in nums.indices {
+            if let ret = dict[target - nums[index]] {
+             return [ret, index]
+            } else {
+                dict[nums[index]] = index
+            }
+        }
+        return []
+    }
+}
+
+let solution = Solution()
+solution.twoSum([2,7,11,15], 9)
+solution.twoSum([3,2,4], 6)
+solution.twoSum([3,3], 6)
